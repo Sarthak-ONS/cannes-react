3268d51d76a9b255fabd42952cee924e4c404422
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-05 17:22:10 +0530

    Added Auth Routes

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,18 @@ require("dotenv").config({
   path: path.join(__dirname, "../", "/vars", ".env"),
 });
 
+const morgan = require("morgan");
 const express = require("express");
+
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(morgan("dev"));
+
+app.use(authRoutes);
 
 app.listen(PORT, () => {
   console.log(`RUNNING ON PORT=${PORT}`);
